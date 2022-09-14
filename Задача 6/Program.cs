d67a9968b86d4e317e26cd9d947ecc8be6e82481
Roman <brv161@gmail.com>
2022-09-14 22:10:10 +0300

    Задача 6

---
+++ Задача 6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите N: ");
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    Console.Write(Math.Pow(i,2) + ", ");
+    i++;
+}
