3656bed06d1bba837056c14653bb068159f29022
Roman <brv161@gmail.com>
2022-09-14 20:00:12 +0300

    Задача 1

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 99)
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine(Convert.ToString(n)[2]);
+
