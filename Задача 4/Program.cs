098086e145efc6d803669487a2705f5323332ad2
Roman <brv161@gmail.com>
2022-09-14 21:48:01 +0300

    Задача 4*

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,14 +1,15 @@
 ﻿Console.WriteLine("Введите номер четверти: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
+while (n < 1 || n > 4)
+{
+    Console.WriteLine("Ошибка.");
+    n = Convert.ToInt32(Console.ReadLine());
+}
 if (n == 1)
     Console.WriteLine("x > 0, y > 0");
-else if (n == 2)
+if (n == 2)
     Console.WriteLine("x < 0, y > 0");
-else if (n == 3)
+if (n == 3)
     Console.WriteLine("x < 0, y < 0");
-else if (n == 4)
+if (n == 4)
     Console.WriteLine("x > 0, y < 0");
-else 
-    Console.WriteLine("Ошибка.");
-    
