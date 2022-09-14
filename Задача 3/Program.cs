52016ea6ccd20b2f0d000bc2676d905b6863fbb6
Roman <brv161@gmail.com>
2022-09-14 20:39:11 +0300

    Задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите x: ");
+Console.Write("x = ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+Console.Write("y =  ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+    Console.WriteLine("1 ЧЕТВЕРТЬ");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2 ЧЕТВЕРТЬ");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3 ЧЕТВЕРТЬ");
+else if (x > 0 && y < 0)
+    Console.WriteLine("4 ЧЕТВЕРТЬ");
+else 
+    Console.WriteLine("Ошибка x и y не могут быть равны нулю по условию задачи!");
+    
