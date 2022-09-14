8ceb2613adf08e238629d50d66a3b6070a34b770
Roman <brv161@gmail.com>
2022-09-14 20:26:07 +0300

    Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max_f = n;
+int max_s = -1;
+while ( n !=0 )
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max_f)
+    {
+        max_s = max_f;
+        max_f = n;
+    }
+    else if (n > max_s)
+    {
+        max_s = n;
+    }
+}
+Console.WriteLine(max_s);
